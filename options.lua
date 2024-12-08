4ae26932ca009bf9bc27b0679428753fcfbcfb94
econaz <563380805@qq.com>
2024-12-08 21:34:52 +0800

    fix

---
+++ options.lua
@@ -0,0 +1,42 @@
+require "nvchad.options"
+
+local opt = vim.opt
+
+opt.cursorlineopt = "both"
+
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+if vim.fn.has "win32" == 1 then
+  opt.shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell"
+  opt.shellcmdflag =
+    "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
+  opt.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+  opt.shellpipe = '2>&1 | %{ "$_" } | tee %s; exit $LastExitCode'
+  opt.shellquote = ""
+  opt.shellxquote = ""
+end
+
+--- Enable OSC 52 for copying to system clipboard in SSH
+-- See: https://github.com/neovim/neovim/issues/28611#issuecomment-2147744670
+if os.getenv "SSH_TTY" then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    -- Try to use OSC 52 escape sequences to copy to system clipboard
+    -- It should not break anything if it’s not supported
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+      ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+    },
+    -- Disable paste since it does not work in some terminals,
+    -- i.e., xterm.js (many app based on Web tech use this)
+    paste = {
+      ["+"] = function(lines)
+        return vim.split(vim.fn.getreg '"', "\n")
+      end,
+      ["*"] = function(lines)
+        return vim.split(vim.fn.getreg '"', "\n")
+      end,
+    },
+  }
+end
